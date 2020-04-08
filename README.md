c82bdc1b3e06bc042ceb8021d74bac07f46abadd
joyerz <zhuoyue.zhong.sodarcar@daimler.com>
2020-04-08 14:30:57 +0800

    init

--- README.md
+++ README.md
@@ -9,11 +9,11 @@ npm install soda-bo-cli -g
 ## 初始化
 ```shell
 sbc init // 在当前目录初始化项目
-// or
-sbc init-yarn // 使用yarn在当前目录初始化项目
+sbc tsinit // 初始化ts项目
 ```
 
 ## 新建page
 ```shell
 sbc page <pageName> // 在src/pages目录下新建"pageName"模块, "pageName"可以接受次级目录比如: "companyManage/city"
+sbc tspage <pageName> // 在src/pages目录下新建<ts>"pageName"模块, "pageName"可以接受次级目录比如: "companyManage/city"
 ```
--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ const clone = require('git-clone')
 const program = require('commander')
 const shell = require('shelljs')
 const colors = require('colors')
+const request = require('request')
+const fs = require('fs')
+const compressing = require('compressing')
 
+function download(uri, filename, callback) {
+  const stream = fs.createWriteStream(filename)
+  request(uri).pipe(stream).on('close', callback)
+}
 
 program
-  .version('0.0.18','-v, --vers', 'output the current version')
+  .version('0.0.19', '-v, --vers', 'output the current version')
   .description('soda-cli命令行工具')
 
 program
@@ -19,14 +26,15 @@ program
     clone(`https://github.com/joyerz/soda-bo-tpl.git`, pwd + '/tmp', null, function() {
       shell.rm('-rf', pwd + `/tmp/.git`)
       console.log((pwd + '/').yellow)
-      shell.mv(  pwd + '/tmp/.*',  pwd + '/')
-      shell.mv(  pwd + '/tmp/*',  pwd + '/')
+      shell.mv(pwd + '/tmp/.*', pwd + '/')
+      shell.mv(pwd + '/tmp/*', pwd + '/')
       shell.rm('-rf', pwd + `/tmp/`)
 
       console.log('正在安装项目...'.green)
-      if (verbose === 'verbose') {
+      if (verbose === 'verbose' || verbose === 'v') {
         shell.exec('yarn install --verbose')
-      } else {
+      }
+      else {
         shell.exec('yarn install')
       }
       console.log('项目初始化完成'.green)
@@ -41,14 +49,15 @@ program
     let pwd = shell.pwd()
     clone(`https://github.com/joyerz/soda-bo-tpl.git`, pwd + '/tmp', null, function() {
       shell.rm('-rf', pwd + `/tmp/.git`)
-      shell.mv(  pwd + '/tmp/.*',  pwd + '/')
-      shell.mv(  pwd + '/tmp/*',  pwd + '/')
+      shell.mv(pwd + '/tmp/.*', pwd + '/')
+      shell.mv(pwd + '/tmp/*', pwd + '/')
       shell.rm('-rf', pwd + `/tmp/`)
 
       console.log('正在安装项目...'.green)
-      if (verbose === 'verbose') {
+      if (verbose === 'verbose' || verbose === 'v') {
         shell.exec('yarn install --verbose')
-      } else {
+      }
+      else {
         shell.exec('yarn install')
       }
       console.log('项目初始化完成'.green)
@@ -62,44 +71,145 @@ program
     console.log('新增页面...'.green)
     const pwd = shell.pwd()
     if (page !== '') {
-      if (page.substring(page.length -1) === '/') {
-        page = page.substring(0, page.length -1)
+      if (page.substring(page.length - 1) === '/') {
+        page = page.substring(0, page.length - 1)
       }
 
       clone(`https://github.com/joyerz/soda-bo-tpl-page.git`, pwd + '/tmp', null, function() {
         shell.rm('-rf', pwd + `/tmp/.git`)
-        shell.mkdir('-p',  `${pwd}/src/pages/${page}/`)
-        shell.mv(`${pwd}/tmp/*`,  `${pwd}/src/pages/${page}/`)
+        shell.mkdir('-p', `${pwd}/src/pages/${page}/`)
+        shell.mv(`${pwd}/tmp/*`, `${pwd}/src/pages/${page}/`)
         shell.rm('-rf', pwd + `/tmp/`)
         console.log('新增页面完成'.green)
       })
-    } else {
+    }
+    else {
       console.log('请输入正确的page, 如 soda-bo-cli page PAGENAME'.white)
     }
   })
 
-
+// 新建ts项目
 // program
-//   .command('com <component>')
-//   .description('添加模块')
-//   .action(function(component) {
-//     console.log('新增组件...'.green)
-//     const pwd = shell.pwd()
-//     if (component !== '') {
-//       if (component.substring(component.length -1) === '/') {
-//         component = component.substring(0, component.length -1)
-//       }
+//   .command('tsinit <verbose>')
+//   .description('初始化ts项目')
+//   .action(function(verbose) {
+//     console.log('初始化ts项目...'.green)
+//     let pwd = shell.pwd()
+//     clone(`https://github.com/joyerz/soda-ts-bo.git`, pwd + '/tmp', null, function() {
+//       console.log(('当前安装目录: ' + pwd + '/').yellow)
+//       shell.rm('-rf', pwd + `/tmp/.git`)
+//       shell.mv(pwd + '/tmp/.*', pwd + '/')
+//       shell.mv(pwd + '/tmp/*', pwd + '/')
+//       shell.rm('-rf', pwd + `/tmp/`)
 //
-//       clone(`https://github.com/joyerz/soda-bo-tpl-page.git`, pwd + '/tmp', null, function() {
-//         shell.rm('-rf', pwd + `/tmp/.git`)
-//         shell.mv(`${pwd}/tmp/.*`,  `${pwd}/components/${component}/`)
-//         shell.mv(`${pwd}/tmp/*`,  `${pwd}/components/${component}/`)
-//         shell.rm('-rf', pwd + `/tmp/`)
-//         console.log('新增页面模块完成...'.green)
-//       })
-//     } else {
-//       console.log('请输入正确的组件名字, 如 soda-bo-cli com COMPONENT'.white)
-//     }
+//       console.log('正在安装项目...'.green)
+//       if (verbose === 'verbose' || verbose === 'v') {
+//         shell.exec('yarn install --verbose')
+//       }
+//       else {
+//         shell.exec('yarn install')
+//       }
+//       console.log('项目初始化完成'.green)
+//     })
 //   })
+program
+  .command('tsinit')
+  .description('初始化ts项目')
+  .action(function(verbose) {
+    console.log('初始化ts项目...'.green)
+    let pwd = shell.pwd()
+
+    const path = pwd + '/tmp'
+    const file = path + '/tmp.zip'
+    shell.mkdir('-p', path)
+    download('https://codeload.github.com/joyerz/soda-ts-bo/zip/master', file, function() {
+      compressing
+        .zip
+        .uncompress(file, path)
+        .then(() => {
+          console.log(('当前安装目录: ' + pwd + '/').yellow)
+          shell.mv(path + '/soda-ts-bo-master/.*', pwd + '/')
+          shell.mv(path + '/soda-ts-bo-master/*', pwd + '/')
+          shell.rm('-rf', pwd + '/src/pages/tpl') // 删除样板目录
+          shell.rm('-rf', path)
+
+          console.log('正在安装项目...'.green)
+          if (verbose === 'verbose' || verbose === 'v') {
+            shell.exec('yarn install --verbose')
+          }
+          else {
+            shell.exec('yarn install')
+          }
+          console.log('项目初始化完成'.green)
+        })
+        .catch(() => {
+          console.log('项目初始化失败!'.red)
+        })
+    })
+
+    // clone(`https://github.com/joyerz/soda-ts-bo.git`, pwd + '/tmp', null, function() {
+    //   console.log(('当前安装目录: ' + pwd + '/').yellow)
+    //   shell.rm('-rf', pwd + `/tmp/.git`)
+    //   shell.mv(pwd + '/tmp/.*', pwd + '/')
+    //   shell.mv(pwd + '/tmp/*', pwd + '/')
+    //   shell.rm('-rf', pwd + `/tmp/`)
+    //
+    //   console.log('正在安装项目...'.green)
+    //   if (verbose === 'verbose' || verbose === 'v') {
+    //     shell.exec('yarn install --verbose')
+    //   }
+    //   else {
+    //     shell.exec('yarn install')
+    //   }
+    //   console.log('项目初始化完成'.green)
+    // })
+  })
+
+// 新建ts模块
+program
+  .command('tspage <page>')
+  .description('添加模块')
+  .action(function(page) {
+    console.log('新增页面...'.green)
+    const pwd = shell.pwd()
+    const path = pwd + '/tmp'
+    const file = path + '/tmp.zip'
+
+    if (page !== '') {
+      if (page.substring(page.length - 1) === '/') {
+        page = page.substring(0, page.length - 1)
+      }
+
+      shell.mkdir('-p', path)
+      download('https://codeload.github.com/joyerz/soda-ts-bo/zip/master', file, function() {
+        compressing
+          .zip
+          .uncompress(file, path)
+          .then(() => {
+            const pagePath = `${pwd}/src/pages/${page}/`
+            shell.mkdir('-p', pagePath)
+            shell.mv(path + '/soda-ts-bo-master/src/pages/tpl/*', pagePath)
+            shell.rm('-rf', path)
+
+            console.log('新增页面完成'.green)
+          })
+          .catch(() => {
+            console.log('新增页面失败!'.red)
+          })
+      })
+
+      // clone(`https://github.com/joyerz/soda-ts-bo.git`, pwd + '/tmp', null, function() {
+      //   shell.rm('-rf', pwd + `/tmp/.git`)
+      //   shell.mkdir('-p', `${pwd}/src/pages/${page}/`)
+      //   shell.mv(`${pwd}/tmp/pages/home/*`, `${pwd}/src/pages/${page}/`)
+      //   shell.rm('-rf', pwd + `/tmp/`)
+      //   console.log('新增页面完成'.green)
+      // })
+    }
+    else {
+      console.log('请输入正确的page, 如 soda-bo-cli page PAGENAME'.white)
+    }
+  })
+
 
 program.parse(process.argv)
