8d27d9f29a6580fec13c6f35b7e1ffb02df87289
姜万伟 <jiangwanwei@jiangmoweideMacBook-Pro.local>
2020-04-08 14:20:56 +0800

    新增taro相关

--- src/index.js
+++ src/index.js
@@ -4,23 +4,25 @@ const clone = require('git-clone')
 const program = require('commander')
 const shell = require('shelljs')
 const colors = require('colors')
-
+const fs = require('fs')
+const path = require('path')
+const TaroPageMaker = require('../tpl/taro')
 
 program
-  .version('0.0.18','-v, --vers', 'output the current version')
+  .version('0.0.18', '-v, --vers', 'output the current version')
   .description('soda-cli命令行工具')
 
 program
   .command('init <verbose>')
   .description('初始化项目')
-  .action(function(verbose) {
+  .action(function (verbose) {
     console.log('初始化项目...'.green)
     let pwd = shell.pwd()
-    clone(`https://github.com/joyerz/soda-bo-tpl.git`, pwd + '/tmp', null, function() {
+    clone(`https://github.com/joyerz/soda-bo-tpl.git`, pwd + '/tmp', null, function () {
       shell.rm('-rf', pwd + `/tmp/.git`)
       console.log((pwd + '/').yellow)
-      shell.mv(  pwd + '/tmp/.*',  pwd + '/')
-      shell.mv(  pwd + '/tmp/*',  pwd + '/')
+      shell.mv(pwd + '/tmp/.*', pwd + '/')
+      shell.mv(pwd + '/tmp/*', pwd + '/')
       shell.rm('-rf', pwd + `/tmp/`)
 
       console.log('正在安装项目...'.green)
@@ -36,13 +38,13 @@ program
 program
   .command('init-yarn <verbose>')
   .description('初始化项目')
-  .action(function(verbose) {
+  .action(function (verbose) {
     console.log('初始化项目...'.green)
     let pwd = shell.pwd()
-    clone(`https://github.com/joyerz/soda-bo-tpl.git`, pwd + '/tmp', null, function() {
+    clone(`https://github.com/joyerz/soda-bo-tpl.git`, pwd + '/tmp', null, function () {
       shell.rm('-rf', pwd + `/tmp/.git`)
-      shell.mv(  pwd + '/tmp/.*',  pwd + '/')
-      shell.mv(  pwd + '/tmp/*',  pwd + '/')
+      shell.mv(pwd + '/tmp/.*', pwd + '/')
+      shell.mv(pwd + '/tmp/*', pwd + '/')
       shell.rm('-rf', pwd + `/tmp/`)
 
       console.log('正在安装项目...'.green)
@@ -58,18 +60,18 @@ program
 program
   .command('page <page>')
   .description('添加模块')
-  .action(function(page) {
+  .action(function (page) {
     console.log('新增页面...'.green)
     const pwd = shell.pwd()
     if (page !== '') {
-      if (page.substring(page.length -1) === '/') {
-        page = page.substring(0, page.length -1)
+      if (page.substring(page.length - 1) === '/') {
+        page = page.substring(0, page.length - 1)
       }
 
-      clone(`https://github.com/joyerz/soda-bo-tpl-page.git`, pwd + '/tmp', null, function() {
+      clone(`https://github.com/joyerz/soda-bo-tpl-page.git`, pwd + '/tmp', null, function () {
         shell.rm('-rf', pwd + `/tmp/.git`)
-        shell.mkdir('-p',  `${pwd}/src/pages/${page}/`)
-        shell.mv(`${pwd}/tmp/*`,  `${pwd}/src/pages/${page}/`)
+        shell.mkdir('-p', `${pwd}/src/pages/${page}/`)
+        shell.mv(`${pwd}/tmp/*`, `${pwd}/src/pages/${page}/`)
         shell.rm('-rf', pwd + `/tmp/`)
         console.log('新增页面完成'.green)
       })
@@ -77,6 +79,41 @@ program
       console.log('请输入正确的page, 如 soda-bo-cli page PAGENAME'.white)
     }
   })
+
+program
+  .command('taroinit <verbose>')
+  .description('初始化项目(Taro)')
+  .action(function (verbose) {
+    let proPath = path.resolve(shell.pwd().toString(), `./${verbose}`)
+    if (fs.existsSync(proPath)) {
+      return console.log(`'${proPath}' 已存在.`.red)
+    }
+    fs.mkdirSync(proPath)
+    shell.cd(proPath)
+    console.log('初始化项目...'.green)
+    let pwd = shell.pwd()
+    clone(`https://github.com/joyerz/soda-taro-base`, pwd + '/tmp', null, function () {
+      shell.rm('-rf', pwd + `/tmp/.git`)
+      console.log((pwd + '/').yellow)
+      shell.mv(pwd + '/tmp/.*', pwd + '/')
+      shell.mv(pwd + '/tmp/*', pwd + '/')
+      shell.rm('-rf', pwd + `/tmp/`)
+
+      console.log('正在安装项目...'.green)
+      if (verbose === 'verbose') {
+        shell.exec('yarn install --verbose')
+      } else {
+        shell.exec('yarn install')
+      }
+      console.log('项目初始化完成'.green)
+      console.log(`\n\n执行命令查看可执行脚本:\n\n$ cd ${verbose} && npm run\n\n`.green)
+    })
+  })
+
+program
+  .command('taropage <page>')
+  .description('添加模块')
+  .action(TaroPageMaker)
 
 
 // program
